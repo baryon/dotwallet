9037bf0422cef13aa3458cb1759c97eacdbc913e
lilong <lilong@gmail.com>
2021-01-12 16:02:17 +0900

    updated package

--- examples/example.js
+++ examples/example.js
@@ -23,11 +23,11 @@ async function createOrder () {
   const orderSN = uuid.v4();//订单号
   const itemPrice = 2000; //商品价格,单位:聪
   const recvAddr = '1QG3jKAt5qt41yVEm7JfMraJ1YZ5WpY8Wx'; //收款地址
-  const itemName = '商品名称:一个苹果';
-  const opreturn = '这是一笔普通订单,购买了一个苹果'; //可以为商量自己组装的交易脚本,此情况下必须为hex 字符串
+  const itemName = '1';//'商品名称:一个苹果';
+  const opreturn = '';//'这是一笔普通订单,购买了一个苹果'; //可以为商量自己组装的交易脚本,此情况下必须为hex 字符串
   let rst = await merchant.CreateOrder( orderSN, itemName, itemPrice, opreturn );
-  console.log( rst.data.redirectURL );
-  console.log( merchant.GetUserPayURL( rst.data.orderSN ) );
+  console.log( rst );
+  console.log( merchant.GetUserPayURL( rst.data.order_sn ) );
 }
 
 
